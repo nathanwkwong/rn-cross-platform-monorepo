c602a0e5dba1bc058102d60e3ead168e28eb758a
Nathan Wong <wkwong.nathan@gmail.com>
2022-02-06 12:36:54 +0800

    [FIX] - modify web package

--- packages/web/src/index.js
+++ packages/web/src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { AppRegistry } from 'react-native'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from 'components/src/App'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+AppRegistry.registerComponent('myprojectname', () => App)
+AppRegistry.runApplication('myprojectname', {
+  rootTag: document.getElementById('root'),
+})
