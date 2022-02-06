159a99ed5aa03e31cecc6cc7546972c758e889fa
Nathan Wong <wkwong.nathan@gmail.com>
2022-02-06 22:39:24 +0800

    [ADD] - add webview

--- packages/components/src/App.js
+++ packages/components/src/App.js
@@ -16,19 +16,51 @@ import {
   Text,
   View,
 } from 'react-native'
-import LogoSrc from './logo.png'
+import { WebView } from 'react-native-webview'
 
 const App = () => {
+  const debugging = `
+  const consoleLog = (type, log) => window.ReactNativeWebView.postMessage(JSON.stringify({'type': 'Console', 'data': {'type': type, 'log': log}}));
+  console = {
+      log: (log) => consoleLog('log', log),
+      debug: (log) => consoleLog('debug', log),
+      info: (log) => consoleLog('info', log),
+      warn: (log) => consoleLog('warn', log),
+      error: (log) => consoleLog('error', log),
+    };
+`
+  // return (
+  //   <View>
+  //     <Text>hi</Text>
+  //     <Text>hi</Text>
+  //     <Text>hi</Text>
+  //     <Text>hi</Text>
+  //     <Text>hi</Text>
+  //     <Text>hi</Text>
+  //   </View>
+  // )
+  const onMessage = (payload) => {
+    let dataPayload
+    console.log(payload)
+    try {
+      dataPayload = JSON.parse(payload.nativeEvent.data)
+    } catch (e) {}
+
+    if (dataPayload) {
+      if (dataPayload.type === 'Console') {
+        console.info(`[Console] ${JSON.stringify(dataPayload.data)}`)
+      } else {
+        console.log(dataPayload)
+      }
+    }
+  }
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Image style={styles.logo} source={LogoSrc} />
-        <View>
-          <Text>Hello from Mono Repoa</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <WebView
+      source={{ uri: 'http://10.0.2.2:3002' }}
+      style={{ marginTop: 20 }}
+      injectedJavaScript={debugging}
+      onMessage={onMessage}
+    />
   )
 }
 
